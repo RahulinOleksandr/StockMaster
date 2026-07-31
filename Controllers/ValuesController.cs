9b6cf26bd40f9f4a9880088c07a43246a6e4c7ea
r0fys <rofysyt@gmail.com>
2026-07-31 21:15:19 +0300

    initialize WebAPI project and add test HTML page

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using First_web_project.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace First_web_project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ValuesController : ControllerBase
+    {
+        [Route("GetJobList")]
+        [HttpGet]
+        public List<Class> GetAllInfo()
+        {
+            List<Class> list = new List<Class>();
+
+            Class _job1 = new Class()
+            {
+                jobId = 0,
+                jobName = ".Net dev",
+                jobDescription = "Web"
+            };
+            list.Add(_job1);
+
+            Class _job2 = new Class()
+            {
+                jobId = 1,
+                jobName = "React",
+                jobDescription = "full stack"
+            };
+            list.Add(_job2);
+
+            Class _job3 = new Class()
+            {
+                jobId = 2,
+                jobName = "C++",
+                jobDescription = "back"
+            };
+            list.Add(_job3);
+
+            Class _job4 = new Class()
+            {
+                jobId = 3,
+                jobName = "C++",
+                jobDescription = "game dev"
+            };
+            list.Add(_job4);
+
+            return list;
+        }
+    }
+}
